6c0b8fe338d08c541002d139d1830d1ff18f8a99
ghayth-1 <ghaith.ouertani@edu.isetcom.tn>
2026-03-08 00:10:52 +0100

    Rename package and update entry point in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ghostpass",
+    name="ghostpass-tool",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "ghostpass=ghostpass_core.cli:main",
+            "ghostpass=ghostpass.cli:main",
         ],
     },
 )
