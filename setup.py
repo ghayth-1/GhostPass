4935614b7b53872f37236a6a22a817d4031151c1
ghayth-1 <ghaith.ouertani@edu.isetcom.tn>
2026-03-08 00:04:56 +0100

    Format setup.py for better readability

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+
 setup(
     name="ghostpass",
     version="1.0.0",
     packages=find_packages(),
-    install_requires=["requests>=2.28.0", "colorama>=0.4.6"],
-    entry_points={"console_scripts": ["ghostpass=ghostpass_core.cli:main"]},
+    install_requires=[
+        "requests>=2.28.0",
+        "colorama>=0.4.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ghostpass=ghostpass_core.cli:main",
+        ],
+    },
 )
